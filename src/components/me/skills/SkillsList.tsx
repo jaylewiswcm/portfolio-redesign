157f812fb7ae189857e92f79049dabe80d6d3dd8
Jay Lewis <jr_lewis@outlook.com>
2021-05-06 10:30:31 +0100

    Skills component added

--- src/components/me/skills/SkillsList.tsx
+++ src/components/me/skills/SkillsList.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+// Svg
+import DownArrow from "../../../images/icons/down-arrow.svg"
 interface ComponentProps {
   data: {
     allSkillsJson : {
@@ -7,14 +8,41 @@ interface ComponentProps {
     }
   }
 }
+
+
  const SkillsList = ({data}: ComponentProps) => {  
+   const [selectedSkillset, setSkillSet] = useState(-1)
+
+   const toggleSkills = (number: number) => {
+     if(number !== selectedSkillset) {
+      setSkillSet(number);
+     } else {
+      setSkillSet(-1);
+     }
+  }
+  
   return (
     <div>
-      <p>Coming very soon</p>
-       {/* {
-         data.allSkillsJson.edges.map((item: any) => 
-        <li key={item.node.type}>{item.node.type}</li>)
-       } */}
+       {
+         data.allSkillsJson.edges.map((item: any , index: number) => 
+        <li key={item.node.type}>
+          <div className="list-top">
+            <p className="skill-category">{item.node.type}</p>
+            <p className="push-right">{selectedSkillset !== index  && item.node.skills[0]}</p>
+            <button onClick={() => toggleSkills(index)}>
+              <img className={selectedSkillset === index ? "up-arrow" : "down-arrow"}src={DownArrow} alt="Arrow"/>
+            </button>
+          </div>
+          <div className={selectedSkillset === index ? "increase-height" :"list-bottom"}
+            >
+            <ul className="category-grid">
+              { item.node.skills.map((skill: string) => 
+                <li><p>{skill}</p></li>
+              ) }
+            </ul>
+          </div>
+        </li>)
+       }
     </div>
   )
 }
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,12 +2,16 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import AnchorLinkBtn from "../components/buttons/AnchorLinkBtn";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="not-found">
+      <h2>404 Page not found</h2>  
+      <p className="not-found-p">Looks like you went down the wrong route. Go back home by clicking below.</p>
+      <AnchorLinkBtn color="blue" url="/" text="Go back home"/>
+    </div> 
   </Layout>
 )
 
